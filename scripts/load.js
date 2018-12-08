a132833e3240eebaf63b16019c649aef095ff3ec
Eric <chouigneux330@hotmail.com>
2018-12-08 14:16:59 -0500

    Update home.html

--- scripts/load.js
+++ scripts/load.js
@@ -1,6 +1,5 @@
 function load() {
-    // post username to server, receive data
-
+    // server response string goes into data
     data = '{"fname":"John", "dname":"Milo", "breed":"Schnauzer", "age":7, "act1":"Sleeping", "act2":"Eating", "act3":"Barking"}';
     obj = JSON.parse(data);
 }
