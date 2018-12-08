2654474329afa92680a2020e763bef7416b34558
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2018-12-08 18:51:15 -0500

    adding scripts to handle friendships

---
+++ pending_requests.php
@@ -0,0 +1,19 @@
+<?php
+$username = "Aeroceli";
+$sqlQuery = "SELECT * FROM dogs WHERE id in (SELECT dog1 FROM friends where dog2 IN (SELECT id From dogs where owner = '$username') AND accepted = false); ";
+try{
+    $result  = $mysqli->query($sqlQuery);
+    $data = NULL;
+    while($row =  $result->fetch_assoc()){
+        $data[] = $row;
+    }
+    if($data == NULL){
+     echo '{"status":false}';
+    }
+    else{
+        $requests = json_encode($data);
+        echo '{"status":true, "requests":'.$requests.'}';
+    }
+}catch (Exception $e) {
+    echo '{"status":false}';
+}
