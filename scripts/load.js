e959fe2c437eb29253b9c993f9e4fde64e7934d6
Eric <chouigneux330@hotmail.com>
2018-12-08 23:13:03 -0500

    Updated redirects

--- scripts/load.js
+++ scripts/load.js
@@ -1,5 +1,6 @@
 function load() {
     // server response string goes into data
-    data = '{"fname":"John", "dname":"Milo", "breed":"Schnauzer", "age":7, "act1":"Sleeping", "act2":"Eating", "act3":"Barking"}';
+    data = '{"uname":"DogLover", "fname":"John", "dname":"Milo", "breed":"Schnauzer", "age":7, "act1":"Sleeping", "act2":"Eating", "act3":"Barking"}';
     obj = JSON.parse(data);
+    document.getElementById("uname").innerHTML = obj.uname;
 }
