99ce4503d846f3da13911d930a6af79afaf5ecc1
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2018-12-09 19:38:46 -0500

    Fixed profile query

--- profile.php
+++ profile.php
@@ -1,5 +1,5 @@
 <?php
-$sqlQuery = "SELECT firstName as fname, Name as dname, breed, age, activity1 as act1, activity2 as act2, activity3, image as act3 FROM users, dogs WHERE users.username = (SELECT uname from sessions where SessionID = '".$_COOKIE['SessionID']."') AND dogs.owner = (SELECT uname from sessions where SessionID = '".$_COOKIE['SessionID']."')";
+$sqlQuery = "SELECT firstName as fname, Name as dname, breed, age, activity1 as act1, activity2 as act2, activity3 as act3, image FROM users, dogs WHERE users.username = (SELECT uname from sessions where SessionID = '".$_COOKIE['SessionID']."') AND dogs.owner = (SELECT uname from sessions where SessionID = '".$_COOKIE['SessionID']."')";
 try{
     $result  = $GLOBALS['mysqli']->query($sqlQuery);
     $data = NULL;
