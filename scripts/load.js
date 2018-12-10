a50bfd91d72bd4f5866fef47250ac556248a0263
Eric <chouigneux330@hotmail.com>
2018-12-09 19:43:55 -0500

    Profile response working

--- scripts/load.js
+++ scripts/load.js
@@ -1,6 +1,9 @@
-function load() {
-    // server response string goes into data
-    data = '{"uname":"DogLover", "fname":"John", "dname":"Milo", "breed":"Schnauzer", "age":7, "act1":"Sleeping", "act2":"Eating", "act3":"Barking"}';
-    obj = JSON.parse(data);
-    document.getElementById("uname").innerHTML = obj.uname;
+var request;
+var data;
+
+function load(PHPscript) {
+    request = new XMLHttpRequest();
+    request.open("POST", PHPscript, true);
+    request.addEventListener("readystatechange", requestReceived, false);
+    request.send('');
 }
