2f88804b5988899087d8473ffd844599ed1b2b04
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2018-12-09 20:03:04 -0500

    Remove field checks in php

--- newdog.php
+++ newdog.php
@@ -2,14 +2,6 @@
 $dogName = $dataSignup['dname'];
 $dogAge = $dataSignup['age'];
 $breed = $dataSignup['breed'];
-//SQL injection;
-if(!checkField($dogName) ||  !checkField($dogAge) || !checkField($breed)){
-    $toRet["status"] = false;
-    $toRet["field"] = "all";
-    $toRet["errorMessage"] = "One of the required fields is empty, probably checked in javascript";
-    echo json_encode($toRet);
-    return;
-}
 try{
     $dogInsertQuery = "INSERT INTO `dogs`(`Name`, `Age`, `owner`, `breed`) VALUES ('$dogName',$dogAge,'$username','$breed');";
     $mysqli->query($dogInsertQuery);    
