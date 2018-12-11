27fb9a0f99519747ebb0c22cf7c66c918fb37efc
Eric <chouigneux330@hotmail.com>
2018-12-11 18:38:52 -0500

    Playdate requests (testing)

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,6 +1,6 @@
 function showProfile(dname, breed, age, act1, act2, act3, image) {
     var title = dname + "'s Profile";
-    var win = window.open("showprofile.html", title, "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=" + screen.width + ",height="+ screen.height);
+    var win = window.open("showprofile.html", 'Profile', "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=" + screen.width + ",height="+ screen.height);
     
     win.onload = function() {
         win.document.getElementById("dname").innerHTML = dname;
@@ -11,5 +11,16 @@ function showProfile(dname, breed, age, act1, act2, act3, image) {
         win.document.getElementById("act2").innerHTML = act2;
         win.document.getElementById("act3").innerHTML = act3;
         win.document.getElementById("photo").setAttribute("src", "uploads/"+image);
+     }      
+}
+
+function schedule(id) {
+    var win = window.open("playdateform.html", '', "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=" + screen.width + ",height="+ screen.height);
+    
+    win.onload = function() {
+        win.document.getElementById("id").innerHTML = id;
+        win.document.getElementById("date").setAttribute('valueAsDate', new Date());
+        win.document.getElementById("date").setAttribute('min', new Date());
+        win.document.getElementById("date").setAttribute('max', "2019-12-31");
      }      
 }
