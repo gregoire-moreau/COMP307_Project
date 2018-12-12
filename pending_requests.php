c31e5762b857b65e786aa435f1e211e9d660063d
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2018-12-11 19:59:46 -0500

    Activities from user's dog

--- pending_requests.php
+++ pending_requests.php
@@ -1,5 +1,5 @@
 <?php
-$dogIDQuery = "SELECT id, name, firstName, location from dogs, users WHERE owner = (SELECT uname FROM sessions WHERE SessionID = ?) AND username = (SELECT uname FROM sessions WHERE SessionID = ?) ;";
+$dogIDQuery = "SELECT id, name, activity1, activity2, activity3, firstName, location from dogs, users WHERE owner = (SELECT uname FROM sessions WHERE SessionID = ?) AND username = (SELECT uname FROM sessions WHERE SessionID = ?) ;";
 try{
     $stmt  = $GLOBALS['mysqli']->prepare($dogIDQuery);
     $stmt->bind_param('ss', $_COOKIE["SessionID"], $_COOKIE["SessionID"]);
@@ -18,6 +18,9 @@ try{
         $location = $data[0]["location"];
         $fname =  $data[0]["firstName"];
         $dogName =  $data[0]["name"];
+        $act1 =  $data[0]["activity1"];
+        $act2 =  $data[0]["activity2"];
+        $act3 =  $data[0]["activity3"];
         $pendingRequestsQuery = "SELECT id, Name as dname, breed, age, activity1 as act1, activity2 as act2, activity3 as act3, image FROM dogs WHERE id in (SELECT dog1 FROM friends WHERE dog2 = $dogID AND accepted=false)";
         $result = $GLOBALS['mysqli']->query($pendingRequestsQuery);
         $data = NULL;
@@ -32,7 +35,7 @@ try{
             $data[] = $row;
         }
         $otherDogs = json_encode($data);
-        echo '{"status":true, "fname":"'.$fname.'", "dname":"'.$dogName.'" , "pendingRequests":'.$requests.', "otherDogs":'.$otherDogs.'}';
+        echo '{"status":true, "fname":"'.$fname.'", "dname":"'.$dogName.'" , "act1":"'.$act1.'", "act2":"'.$act2.'","act3":"'.$act3.'", "pendingRequests":'.$requests.', "otherDogs":'.$otherDogs.'}';
     }
 
     
