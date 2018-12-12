59556aed46da6c36746aea3ae589fa397d2789d7
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2018-12-11 19:26:58 -0500

    Update accept_playdate.php

--- accept_playdate.php
+++ accept_playdate.php
@@ -1,7 +1,7 @@
 <?php
-$checkQuery = "SELECT accepted FROM playdates WHERE dog2 = (SELECT id FROM dogs WHERE owner = (SELECT uname FROM sessions WHERE SessionID = ?)) AND dog1=?;";
+$checkQuery = "SELECT accepted FROM playdates WHERE dog2 = (SELECT id FROM dogs WHERE owner = (SELECT uname FROM sessions WHERE SessionID = ?)) AND dog1=? AND date = ?;";
 $stmt =  $GLOBALS['mysqli']->prepare($checkQuery);
-$stmt->bind_param('sd',$_COOKIE['SessionID'], $dogData["dogID"] );
+$stmt->bind_param('sds',$_COOKIE['SessionID'], $dogData["dogID"], $dogData["date"] );
 $stmt->execute();
 $result = $stmt->get_result();
 $data = NULL;
@@ -13,15 +13,15 @@ if ($dogData["answer"] == "true"){
         echo '{"status":false}';
         return;
     }
-    $query = "UPDATE playdates SET accepted = true WHERE dog2 = (SELECT id FROM dogs WHERE owner = (SELECT uname FROM sessions WHERE SessionID = ?)) AND dog1=?;";
+    $query = "UPDATE playdates SET accepted = true WHERE dog2 = (SELECT id FROM dogs WHERE owner = (SELECT uname FROM sessions WHERE SessionID = ?)) AND dog1=? AND date=?;";
 }else{
     if($data == NULL || $data[0]["accepted"]){
         echo '{"status":false}';
         return;
     }
-    $query = "DELETE FROM playdates WHERE dog2 = (SELECT id FROM dogs WHERE owner = (SELECT uname FROM sessions WHERE SessionID = ?)) AND dog1=?;";
+    $query = "DELETE FROM playdates WHERE dog2 = (SELECT id FROM dogs WHERE owner = (SELECT uname FROM sessions WHERE SessionID = ?)) AND dog1=? AND date=?;";
 }
 $stmt =  $GLOBALS['mysqli']->prepare($query);
-$stmt->bind_param('sd',$_COOKIE['SessionID'], $dogData["dogID"] );
+$stmt->bind_param('sds',$_COOKIE['SessionID'], $dogData["dogID"] ,$dogData["date"] );
 $stmt->execute();
 echo '{"status":true}';
